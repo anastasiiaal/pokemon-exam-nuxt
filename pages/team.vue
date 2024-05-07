4965c3b4be1f258e6f1809b4181a402489bdcc97
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-05-07 16:44:35 +0200

    style team wrapper

--- pages/team.vue
+++ pages/team.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <h2 class="text-center text-xl mb-10 font-bold">My team</h2>
 
-        <div class="wrapper">
+        <div class="wrapper-team">
             <PokemonCardTeam v-for="pokemon in team" :key="pokemon.id" :pokemon="pokemon"/>
         </div>
     </div>
