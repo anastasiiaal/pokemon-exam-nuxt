a4ec9aa839c9e4197d8c888cb6c26932e330902f
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-05-07 16:26:42 +0200

    refactor fetches

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,17 @@
 
         <h1 class="text-xl font-bold text-center">Catalog</h1>
 
-        <div class="flex flex-wrap justify-center gap-4 mt-5">
+        <h2 v-if="pendingTypes" class="w-full text-center text-xl font-bold my-5">Loading types ...</h2>
+        <div v-else class="flex flex-wrap justify-center gap-4 mt-5">
             <button v-for="type in types" :key="type.id" class="px-4 py-2 bg-blue-200 cursor-pointer rounded-sm">
                 {{ type.name }}
             </button>
-            
         </div>
 
-        <div class="wrapper">
+        <div v-if="pendingPokemons" class="wrapper">
+            <h2 class="w-full text-center text-xl font-bold">Loading pokes ...</h2>
+        </div>
+        <div v-else class="wrapper">
             <PokemonCard v-for="pokemon in pokemons" :key="pokemon.id" :pokemon="pokemon" />
         </div>
     
@@ -18,27 +21,10 @@
 </template>
 
 <script setup>
-import PokemonCard from '~/components/PokemonCard.vue';
+    import PokemonCard from '~/components/PokemonCard.vue';
 
-
-    const { getPokemons, getCategories } = useApi();
-    const pokemons = ref([]);
-    const types = ref([])
-
-    async function getAllPokemons () {
-        pokemons.value = await getPokemons();
-    }
-
-    async function getAllCategories () {
-        types.value = await getCategories();
-    }
-
-    onMounted(() => {
-            getAllPokemons()
-            getAllCategories()
-        }
-    )
-
+    const { data: pokemons, pending: pendingPokemons } = useFetch(`https://pokebuildapi.fr/api/v1/pokemon/`);
+    const { data: types, pending: pendingTypes } = useFetch(`https://pokebuildapi.fr/api/v1/types/`);
 </script>
 
 <style scoped>
